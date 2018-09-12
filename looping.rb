55f3d1bea6e9cfaf8903a35eaee1958ae36c100a
J Fern <jfern20@gmail.com>
2018-09-12 03:05:05 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,8 +1,16 @@
 def looping
 loop do 
+<<<<<<< HEAD
 puts"Wingardium Leviosa"
 end
 end 
 looping
 
 
+=======
+puts "Wingardium Leviosa"
+end
+looping
+
+end 
+>>>>>>> cc97a1e7612d08b368229a92535d07d87aa054f9
